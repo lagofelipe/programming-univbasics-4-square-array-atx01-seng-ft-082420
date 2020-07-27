85d7e255d83d406d8c593eb5f11701ffe2eeca58
Felipe Dolago <lagofelipe@gmail.com>
2020-07-27 00:00:17 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
 counter = 0
   new_array = []
   while counter <  array[counter].length do
-new_array.push(counter ** array[counter])
+new_array.push(counter * array[counter])
   end
   return  new_array
 end
